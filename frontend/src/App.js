d8289d0d84dfa626ea5ee4d8eaae9661e20f7fcb
Noah Rodriguez <nrodrig3@valpo.edu>
2022-09-20 16:59:59 -0500

    Decided to use bootstrap and added a few things

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <>
+        <Header />
+        <main className="my-3">
+            <Container>
+                <HomeScreen />
+            </Container>
+        </main>
+        <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
